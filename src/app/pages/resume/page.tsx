e2e0b64a9f49b7383792bfd0511e125d35008eaf
Jami Yashwanth Kumar <jamiyashwanthkumar@Jamis-MacBook-Air.local>
2024-04-21 02:18:11 +0530

    changes

--- src/app/pages/resume/page.tsx
+++ src/app/pages/resume/page.tsx
@@ -1,6 +1,7 @@
 export default function Resume() {
     return (
         <main className={`flex mt-24 h-full flex-col items-center justify-center `}>
+            <h1>resume</h1>
             <iframe
                 src="https://jamiyashwanth.github.io/portfolio/src/app/pages/resume/jamiyashwanthresume.pdf"
                 width="90%"
